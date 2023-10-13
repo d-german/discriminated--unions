d468831d5f2c6fdbf009aea6f39876dcac6b8e1a
Damon German <damon.german@hyland.com>
2023-10-13 17:04:57 -0500

    Add async support to MaybeExtensions
    Added an asynchronous Bind method to the MaybeExtensions class, to allow for async operations in LINQ query chains. Also fixed an issue where the switch statement didn't handle Something<T> cases when T is a primitive type.

--- Discriminated Unions CI/MaybeExtensions.cs
+++ Discriminated Unions CI/MaybeExtensions.cs
@@ -9,12 +9,36 @@ public static class MaybeExtensions
             Maybe<TOut> updatedValue = @this switch
             {
                 Something<TIn> s when !EqualityComparer<TIn>.Default.Equals(s.Value, default) => new Something<TOut>(f(s.Value)),
+                Something<TIn> s when s.GetType().GetGenericArguments()[0].IsPrimitive => new Something<TOut>(f(s.Value)),
                 Something<TIn> _ => new Nothing<TOut>(),
                 Nothing<TIn> _ => new Nothing<TOut>(),
                 Error<TIn> e => new Error<TOut>(e.CapturedError),
                 _ => new Error<TOut>(new Exception(
                     "New Maybe state that isn't coded for!: " +
                     @this.GetType()))
+            };
+            return updatedValue;
+        }
+        catch (Exception e)
+        {
+            return new Error<TOut>(e);
+        }
+    }
+
+    public static async Task<Maybe<TOut>> BindAsync<TIn, TOut>(this Maybe<TIn> @this, Func<TIn, Task<TOut>> f)
+    {
+        try
+        {
+            Maybe<TOut> updatedValue = @this switch
+            {
+                Something<TIn> s when !EqualityComparer<TIn>.Default.Equals(s.Value, default) => new Something<TOut>(await f(s.Value)),
+                Something<TIn> s when s.GetType().GetGenericArguments()[0].IsPrimitive => new Something<TOut>(await f(s.Value)),
+                Something<TIn> _ => new Nothing<TOut>(),
+                Nothing<TIn> _ => new Nothing<TOut>(),
+                Error<TIn> e => new Error<TOut>(e.CapturedError),
+                _ => new Error<TOut>(
+                    new Exception("New Maybe state that isn't coded for!: " +
+                                  @this.GetType()))
             };
             return updatedValue;
         }
