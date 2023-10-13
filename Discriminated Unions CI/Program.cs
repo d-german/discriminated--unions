515e50a46c91bec9a29314bb6d79a436a5f20fb0
Damon German <damon.german@hyland.com>
2023-10-13 14:08:04 -0500

    Initial commit

---
+++ Discriminated Unions CI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Discriminated_Unions_CI // Note: actual namespace depends on the project name.
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
