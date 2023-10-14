833b488e8dc0fe27aa39ccabb0575759e9d24856
Damon German <damon.german@hyland.com>
2023-10-14 18:15:01 -0500

    Add overloads to Bind function for different types
    Updated the Bind function in MaybeExtensions to support reference types, non-nullable value types, and nullable value types with the BindRef, BindVal, and BindNullable methods respectively. This is to ensure that our generic Maybe Monad behaves correctly for all types of data. This change will lead to less unexpected behavior, making the Monad more robust and reliable. Tests have been added to ensure that the newly implemented behavior works as expected.

--- Discriminated Unions CI/MaybeExtensions.cs
+++ Discriminated Unions CI/MaybeExtensions.cs
@@ -2,17 +2,64 @@ namespace Discriminated_Unions_CI;
 
 public static class MaybeExtensions
 {
-    public static Maybe<TOut> Bind<TIn, TOut>(this Maybe<TIn> @this, Func<TIn, TOut> f)
+ // Overload for reference types
+    public static Maybe<TOut> BindRef<TIn, TOut>(this Maybe<TIn> @this, Func<TIn, TOut> f) where TIn : class
     {
         try
         {
             Maybe<TOut> updatedValue = @this switch
             {
-                Something<TIn> s when !EqualityComparer<TIn>.Default.Equals(s.Value, default) => new Something<TOut>(f(s.Value)),
-                Something<TIn> s when s.GetType().GetGenericArguments()[0].IsPrimitive => new Something<TOut>(f(s.Value)),
+                Something<TIn> s when s.Value != null => new Something<TOut>(f(s.Value)),
                 Something<TIn> _ => new Nothing<TOut>(),
                 Nothing<TIn> _ => new Nothing<TOut>(),
                 Error<TIn> e => new Error<TOut>(e.CapturedError),
+                _ => new Error<TOut>(new Exception(
+                    "New Maybe state that isn't coded for!: " +
+                    @this.GetType()))
+            };
+            return updatedValue;
+        }
+        catch (Exception e)
+        {
+            return new Error<TOut>(e);
+        }
+    }
+
+    // Overload for non-nullable value types (which includes primitive types)
+    public static Maybe<TOut> BindVal<TIn, TOut>(this Maybe<TIn> @this, Func<TIn, TOut> f) where TIn : struct
+    {
+        try
+        {
+            Maybe<TOut> updatedValue = @this switch
+            {
+                Something<TIn> s when !EqualityComparer<TIn>.Default.Equals(s.Value, default) || s.GetType().GetGenericArguments()[0].IsPrimitive => 
+                new Something<TOut>(f(s.Value)),
+                Something<TIn> _ => new Nothing<TOut>(),
+                Nothing<TIn> _ => new Nothing<TOut>(),
+                Error<TIn> e => new Error<TOut>(e.CapturedError),
+                _ => new Error<TOut>(new Exception(
+                    "New Maybe state that isn't coded for!: " +
+                    @this.GetType()))
+            };
+            return updatedValue;
+        }
+        catch (Exception e)
+        {
+            return new Error<TOut>(e);
+        }
+    }
+
+    // Overload for nullable value types
+    public static Maybe<TOut> BindNullable<TIn, TOut>(this Maybe<TIn?> @this, Func<TIn, TOut> f) where TIn : struct
+    {
+        try
+        {
+            Maybe<TOut> updatedValue = @this switch
+            {
+                Something<TIn?> s when s.Value.HasValue => new Something<TOut>(f(s.Value.Value)),
+                Something<TIn?> _ => new Nothing<TOut>(),
+                Nothing<TIn?> _ => new Nothing<TOut>(),
+                Error<TIn?> e => new Error<TOut>(e.CapturedError),
                 _ => new Error<TOut>(new Exception(
                     "New Maybe state that isn't coded for!: " +
                     @this.GetType()))
--- DiscriminatedUnionsTests/MaybeExtensionsTests.cs
+++ DiscriminatedUnionsTests/MaybeExtensionsTests.cs
@@ -9,7 +9,7 @@ public class MaybeExtensionsTests
     public void Bind_Something_ReturnsTransformedValue()
     {
         var something = new Something<int>(5);
-        var result = something.Bind(Increment);
+        var result = something.BindVal(Increment);
 
         Assert.That(result, Is.TypeOf<Something<int>>());
         var somethingResult = result as Something<int>;
@@ -21,9 +21,9 @@ public class MaybeExtensionsTests
     {
         var something = new Something<int>(5);
         var result = something
-            .Bind(Increment)
-            .Bind(Increment)
-            .Bind(Increment);
+            .BindVal(Increment)
+            .BindVal(Increment)
+            .BindVal(Increment);
 
         Assert.That(result, Is.TypeOf<Something<int>>());
         var somethingResult = result as Something<int>;
@@ -34,7 +34,7 @@ public class MaybeExtensionsTests
     public void Bind_Nothing_ReturnsNothing()
     {
         var nothing = new Nothing<int>();
-        var result = nothing.Bind(Increment);
+        var result = nothing.BindVal(Increment);
 
         Assert.That(result, Is.TypeOf<Nothing<int>>());
     }
@@ -43,7 +43,7 @@ public class MaybeExtensionsTests
     public void Bind_Error_ReturnsError()
     {
         var error = new Error<int>(new Exception("Original error"));
-        var result = error.Bind(Increment);
+        var result = error.BindVal(Increment);
 
         Assert.That(result, Is.TypeOf<Error<int>>());
         var errorResult = result as Error<int>;
@@ -53,5 +53,68 @@ public class MaybeExtensionsTests
     private static int Increment(int value)
     {
         return value + 1;
+    }
+
+    [Test]
+    public void BindNullable_SomethingWithNonNullValue_ReturnsTransformedValue()
+    {
+        var something = new Something<int?>(5);
+        var result = something.BindNullable(Increment);
+
+        Assert.That(result, Is.TypeOf<Something<int>>());
+        var somethingResult = result as Something<int>;
+        Assert.That(somethingResult?.Value, Is.EqualTo(6));
+    }
+
+    [Test]
+    public void BindNullable_SomethingWithNullValue_ReturnsNothing()
+    {
+        var something = new Something<int?>(null);
+        var result = something.BindNullable(Increment);
+
+        Assert.That(result, Is.TypeOf<Nothing<int>>());
+    }
+
+    [Test]
+    public void BindNullable_Nothing_ReturnsNothing()
+    {
+        var nothing = new Nothing<int?>();
+        var result = nothing.BindNullable(Increment);
+
+        Assert.That(result, Is.TypeOf<Nothing<int>>());
+    }
+
+    [Test]
+    public void BindRef_SomethingWithNonNullValue_ReturnsTransformedValue()
+    {
+        var something = new Something<string>("hello");
+        var result = something.BindRef(AppendWorld);
+
+        Assert.That(result, Is.TypeOf<Something<string>>());
+        var somethingResult = result as Something<string>;
+        Assert.That(somethingResult?.Value, Is.EqualTo("hello world"));
+    }
+
+    [Test]
+    public void BindRef_SomethingWithNullValue_ReturnsNothing()
+    {
+        var something = new Something<string>(null);
+        var result = something.BindRef(AppendWorld);
+
+        Assert.That(result, Is.TypeOf<Nothing<string>>());
+    }
+
+    [Test]
+    public void BindRef_Nothing_ReturnsNothing()
+    {
+        var nothing = new Nothing<string>();
+        var result = nothing.BindRef(AppendWorld);
+
+        Assert.That(result, Is.TypeOf<Nothing<string>>());
+    }
+
+    private static string AppendWorld(string value)
+    {
+        return value + " world";
     }
 }
